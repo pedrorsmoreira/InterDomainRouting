3af39abe1bd5fb4a435c46585c40bbff74a69b8c
diogoegrodrigues <diogoegrodrigues@gmail.com>
2019-11-26 15:24:31 +0000

    ola

--- heaps.c
+++ heaps.c
@@ -18,13 +18,14 @@
 #include "heaps.h"
 
 /* A heap is represented by a structure: */
-struct _heap {
-  int (*less) (Item, Item); /* this field is a function pointer to elements in the heap. */
-  int n_elements;           /* # elements in heap. */
-  int size;                 /* max size of the heap. */
-  Item *heapdata;           /* An array of Items. */
+struct _heap
+{
+	int (*less) (Item, Item); /* this field is a function pointer to elements in the heap. */
+	int n_elements;           /* # elements in heap. */
+	int size;                 /* max size of the heap. */
+	Item *heapdata;           /* An array of Items. */
 #ifdef WMspecs
-  int *elements_pos;        /* An array holding the elements position in the heapdata*/
+	int *elements_pos;        /* An array holding the elements position in the heapdata*/
 #endif
 };
 
@@ -42,25 +43,26 @@ struct _heap {
 
 Heap *NewHeap(int size, int (*less) (Item, Item))
 {
-  Heap *h;
+	Heap *h;
+	h = (Heap *) malloc(sizeof(Heap));
+	if (h == ((Heap *) NULL)) exit(0);
 
-  h = (Heap *) malloc(sizeof(Heap));
-  if (h == ((Heap *) NULL))		exit(0);
-
-  h->n_elements = 0;
-  h->less = less;
-  h->size = size;
-  h->heapdata = (Item *) malloc(size * sizeof(Item));
-  if (h->heapdata == ((Item *) NULL))	exit(0);
+	h->n_elements = 0;
+	h->less = less;
+	h->size = size;
+	h->heapdata = (Item *) malloc(size * sizeof(Item));
+	if (h->heapdata == ((Item *) NULL)) exit(0);
 #ifdef WMspecs
-  h->elements_pos = (int *) malloc(size * sizeof(int));
-  if (h->heapdata == NULL)	exit(0);
+	h->elements_pos = (int *) malloc(size * sizeof(int));
+	if (h->heapdata == NULL) exit(0);
 #endif
-  return (h);
+
+	return (h);
 }
 
 #ifdef WMspecs
-int *getHeapElementes_pos(Heap *h){
+int *getHeapElementes_pos(Heap *h)
+{
 	return h->elements_pos;
 }
 #endif
@@ -79,20 +81,21 @@ int *getHeapElementes_pos(Heap *h){
 
 void FixUp(Heap * h, int k)
 {
-  int aux;
-  Item t;
-  while ((k > 0) && (h->less) ((h->heapdata)[(k - 1) / 2], (h->heapdata)[k])) {
-    t = (h->heapdata)[k];
-    (h->heapdata)[k] = (h->heapdata)[(k - 1) / 2];
-    (h->heapdata)[(k - 1) / 2] = t;
+	int aux;
+	Item t;
+	while ((k > 0) && (h->less) ((h->heapdata)[(k - 1) / 2], (h->heapdata)[k])) {
+		t = (h->heapdata)[k];
+		(h->heapdata)[k] = (h->heapdata)[(k - 1) / 2];
+		(h->heapdata)[(k - 1) / 2] = t;
 #ifdef WMspecs
 	   aux = (h->elements_pos)[*((int*)((h->heapdata)[k]))];
 	   (h->elements_pos)[*((int*)((h->heapdata)[k]))] = (h->elements_pos)[*((int*)((h->heapdata)[(k - 1) / 2]))];
 	   (h->elements_pos)[*((int*)((h->heapdata)[(k - 1) / 2]))] = aux;
 #endif
-    k = (k - 1) / 2;
-  }
-  return;
+	   k = (k - 1) / 2;
+	}
+
+	return;
 }
 
 /******************************************************************************
@@ -109,35 +112,34 @@ void FixUp(Heap * h, int k)
 
 void FixDown(Heap * h, int k)
 {
-  int j, aux;
-  Item t;
+	int j, aux;
+	Item t;
 
-  while ((2 * k + 1) < h->n_elements) {
-    j = 2 * k + 1;
-    if (((j + 1) < h->n_elements) &&
-        (h->less) (h->heapdata[j], h->heapdata[j + 1])) {
-      /* second offspring is greater */
-      j++;
-    }
-    if (!(h->less) (h->heapdata[k], h->heapdata[j])) {
-      /* Elements are in correct order. */
-      break;
-    }
+	while ((2 * k + 1) < h->n_elements) {
+		j = 2 * k + 1;
+		if (((j + 1) < h->n_elements) && (h->less) (h->heapdata[j], h->heapdata[j + 1])) {
+			/* second offspring is greater */
+			j++;
+		}
+		if (!(h->less) (h->heapdata[k], h->heapdata[j])) {
+			/* Elements are in correct order. */
+			break;
+		}
 
-    /* the 2 elements are not correctly sorted, it is
-       necessary to exchange them */
-    t = (h->heapdata)[k];
-    (h->heapdata)[k] = (h->heapdata)[j];
-    (h->heapdata)[j] = t;
+		/* the 2 elements are not correctly sorted, it is
+			necessary to exchange them */
+		t = (h->heapdata)[k];
+		(h->heapdata)[k] = (h->heapdata)[j];
+		(h->heapdata)[j] = t;
 #ifdef WMspecs
-	aux = (h->elements_pos)[*((int*)((h->heapdata)[k]))];
-	(h->elements_pos)[*((int*)((h->heapdata)[k]))] = (h->elements_pos)[*((int*)((h->heapdata)[j]))];
-	(h->elements_pos)[*((int*)((h->heapdata)[j]))] = aux;
+		aux = (h->elements_pos)[*((int*)((h->heapdata)[k]))];
+		(h->elements_pos)[*((int*)((h->heapdata)[k]))] = (h->elements_pos)[*((int*)((h->heapdata)[j]))];
+		(h->elements_pos)[*((int*)((h->heapdata)[j]))] = aux;
 #endif	
-    k = j;
-  }
+		k = j;
+	}
 
-  return;
+	return;
 }
 
 /******************************************************************************
@@ -155,8 +157,8 @@ void FixDown(Heap * h, int k)
 
 Item RemoveMax(Heap * h)
 {
-  int aux;
-  Item t;
+	int aux;
+	Item t;
 
     t = (h->heapdata)[0];
     (h->heapdata)[0] = (h->heapdata)[-- (h->n_elements)];
@@ -165,8 +167,10 @@ Item RemoveMax(Heap * h)
 	aux = (h->elements_pos)[*((int*)((h->heapdata)[0]))];
 	(h->elements_pos)[*((int*)((h->heapdata)[0]))] = (h->elements_pos)[*((int*)((h->heapdata)[h->n_elements]))];
 	(h->elements_pos)[*((int*)((h->heapdata)[h->n_elements]))] = aux;
-#endif	
+#endif
+
     FixDown(h, 0);
+
     return t;
 }
 
@@ -184,11 +188,11 @@ Item RemoveMax(Heap * h)
 
 int Direct_Insert(Heap * h, Item element)
 {
-  if (h->n_elements == h->size)		return 0;
+	if (h->n_elements == h->size) return 0;
 
-  h->heapdata[h->n_elements ++] = element;
+	h->heapdata[h->n_elements ++] = element;
 
-  return 1;
+	return 1;
 }
 
 /******************************************************************************
@@ -202,7 +206,8 @@ int Direct_Insert(Heap * h, Item element)
  *
  *****************************************************************************/
 
-void CleanHeap(Heap * h){
+void CleanHeap(Heap * h)
+{
 	for (; h->n_elements > 0; free(h->heapdata[-- h->n_elements]));
 }
 
@@ -221,10 +226,10 @@ void CleanHeap(Heap * h){
 
 void FreeHeap(Heap * h)
 {
-   for (; h->n_elements > 0; free(h->heapdata[-- h->n_elements])); /*Clean Heap*/
-   free(h->heapdata);
+	for (; h->n_elements > 0; free(h->heapdata[-- h->n_elements])); /*Clean Heap*/
+	free(h->heapdata);
 #ifdef WMspecs
-   free(h->elements_pos);
+	free(h->elements_pos);
 #endif
-   free(h);
+	free(h);
 }
